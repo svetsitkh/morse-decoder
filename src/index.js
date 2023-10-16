6a2546101b9a2c9b774199d6a136d02a420552b9
svetsitkh <svetsitkh@gmail.com>
2023-10-16 22:17:47 +0300

    done task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let result = '';
+
+    for (let i = 10; i <= expr.length; i += 10) {
+
+        const binaryLetter = expr.slice(i - 10, i);
+
+        if (binaryLetter == '**********') {
+
+            result = result + ' ';
+
+        } else {
+
+            let morseLetter = '';
+
+            for (let n = 0; n < binaryLetter.length; n += 2) {
+                const letterPart = binaryLetter.slice(n, n + 2);
+                if (letterPart === '10') {
+                    morseLetter = morseLetter + '.';
+                } else if (letterPart === '11') {
+                    morseLetter = morseLetter + '-';
+                }
+            }
+
+            const letter = MORSE_TABLE[morseLetter];
+
+            if (letter) {
+                result = result + letter;
+            }
+
+        }
+    }
+
+    return result;
 }
 
 module.exports = {
     decode
 }
+
+// res = decode('00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010');
+// console.log('result: ', res);
